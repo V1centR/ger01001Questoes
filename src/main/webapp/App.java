1fb686ad37cb0ff93361afd84eab1b245297a016
Vicent <vicent@localhost.localdomain>
2019-05-16 13:51:56 -0400

    set App initial class for export

---
+++ src/main/webapp/App.java
@@ -0,0 +1,13 @@
+package main.webapp;
+
+import br.com.ger1001Questoes.components.GenerateExam;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		GenerateExam gen = new GenerateExam();
+		gen.disciplina(5);
+	}
+
+}
