a7ba1993eec24674007032ce3edb2084a8742b64
Vicent <vicent@localhost.localdomain>
2019-05-17 13:29:24 -0400

    set App main class path

--- src/main/webapp/App.java
+++ src/main/webapp/App.java
@@ -1,9 +1,12 @@
 package main.webapp;
 
+import javax.annotation.PostConstruct;
+
 import br.com.ger1001Questoes.components.GenerateExam;
 
 public class App {
 
+	@PostConstruct
 	public static void main(String[] args) {
 		
 		GenerateExam gen = new GenerateExam();
